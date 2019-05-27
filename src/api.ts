d5d34c5dd24dedab135fff444149f4830d9a83be
axetroy <troy450409405@gmail.com>
2019-05-27 17:51:40 +0800

    fix douban api

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { window } from "vscode";
 
 const headers = {
-  Referer: 'https://movie.douban.com/',
+  Referer: "https://movie.douban.com/",
   Accept:
     "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
   "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,zh-HK;q=0.7",
@@ -11,26 +11,33 @@ const headers = {
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"
 };
 
-function errorHandler(err: any) {
-  const { response } = err;
-  const data = response.data;
+const http = axios.create({
+  headers,
+  params: {
+    apikey: "0df993c66c0c636e29ecbb5344252a4a"
+  }
+});
+
+function errorHandler(err: AxiosError) {
+  const { response, config } = err;
+  const data = (response as any).data;
   if (data && data.msg) {
-    const msg = "请求过于频繁，请稍后重试.";
+    const msg = data.msg;
     window.showErrorMessage(msg);
-    console.dir(err);
+    console.error(`[${(config.method || "GET").toUpperCase()}]: ${config.url}`);
+    console.error(config);
     return Promise.reject(err);
   }
   return Promise.reject(err);
 }
 
 export async function getHotMovie(city: string): Promise<any[]> {
-  const { data } = await axios
+  const { data } = await http
     .get("https://api.douban.com/v2/movie/in_theaters", {
       params: {
         city,
         count: 100
-      },
-      headers: headers
+      }
     })
     .catch(errorHandler);
 
@@ -41,13 +48,12 @@ export async function getTopMovie(
   initStart: number = 0,
   movie: any[] = []
 ): Promise<any[]> {
-  const { data } = await axios
+  const { data } = await http
     .get("https://api.douban.com/v2/movie/top250", {
       params: {
         start: initStart,
         count: 100
-      },
-      headers: headers
+      }
     })
     .catch(errorHandler);
 
@@ -63,12 +69,11 @@ export async function getTopMovie(
 }
 
 export async function getUpcomingMovie(): Promise<any[]> {
-  const { data } = await axios
+  const { data } = await http
     .get("https://api.douban.com/v2/movie/coming_soon", {
       params: {
         count: 100
-      },
-      headers: headers
+      }
     })
     .catch(errorHandler);
 
@@ -76,15 +81,14 @@ export async function getUpcomingMovie(): Promise<any[]> {
 }
 
 export async function getTv(tag: string): Promise<any[]> {
-  const { data } = await axios
+  const { data } = await http
     .get("https://movie.douban.com/j/search_subjects", {
       params: {
         type: "tv",
         tag,
         page_limit: 100,
         page_start: 0
-      },
-      headers: headers
+      }
     })
     .catch(errorHandler);
 
@@ -92,9 +96,11 @@ export async function getTv(tag: string): Promise<any[]> {
 }
 
 export async function getTvTag(): Promise<string[]> {
-  const { data } = await axios
-    .get("https://movie.douban.com/j/search_tags?type=tv", {
-      headers: headers
+  const { data } = await http
+    .get("https://movie.douban.com/j/search_tags", {
+      params: {
+        type: "tv"
+      }
     })
     .catch(errorHandler);
 
